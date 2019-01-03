aab8b90f818c0ce53cac0cb62b0d8477c432b465
Felipe Lema <felipelema@mortemale.org>
2019-01-03 10:38:39 -0300

    State work to be done

---
+++ global.el
@@ -0,0 +1,35 @@
+;;; global.el --- elisp wrapper functions and Emacs integration for GNU global  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Felipe Lema
+
+;; Author: Felipe Lema <felipel@cl01cats24>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; WIP:
+;; - wrapper calls
+;; - project.el integration
+;; - xref.el integration
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'global)
+;;; global.el ends here
