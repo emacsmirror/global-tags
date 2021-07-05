7f0b03af69eb935bd451c8f7fd9b1c6cfed548e1
Felipe Lema <felipelema@mortemale.org>
2021-07-05 16:24:15 -0400

    update documentation to include Imenu

--- global-tags.el
+++ global-tags.el
@@ -41,6 +41,8 @@
 ;; (add-to-list 'xref-backend-functions 'global-tags-xref-backend)
 ;; ;; project.el (finding files)
 ;; (add-to-list 'project-find-functions 'global-tags-try-project-root)
+;; ;; configure Imenu
+;; (add-hook 'ruby-mode-hook #'global-tags-imenu-mode)
 ;; ;; to update database after save
 ;; (add-hook 'c++-mode-hook (lambda ()
 ;;                            (add-hook 'after-save-hook
